79b0b0f6999e2bfbc1c847c5c6e74d63239350ee
Lucas Oliveira <lucasoliveira.fn@gmail.com>
2022-04-19 22:08:19 -0300

    chore: implement storybook configuration

---
+++ .storybook/theme.js
@@ -0,0 +1,11 @@
+import { create } from '@storybook/theming';
+
+const themeLight = create({
+  base: 'light',
+  colorPrimary: '#001529',
+  colorSecondary: '#002140',
+  brandTitle: 'Developer WEB Dashboard',
+  brandUrl: './',
+});
+
+export { themeLight };
